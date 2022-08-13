91b642d7f3f6c7ac6dcd87d123c8a3d9348db907
hohnuki <ohnukihiroki8585@yahoo.co.jp>
2022-08-13 21:50:58 +0900

    [delete]:lexerを再実装のために消した

--- includes/lexer.h
+++ includes/lexer.h
@@ -3,6 +3,7 @@
 
 #include "minishell.h"
 #include "parser.h"
+
 typedef enum e_token_type
 {
 	CHAR_GENERAL		= -1,
@@ -13,7 +14,7 @@ typedef enum e_token_type
 	CHAR_GREATER		= '>',
 	CHAR_LESSER			= '<',
 	TOKEN				= -1,
-}		t_token_type;
+}	t_token_type;
 
 typedef enum e_status
 {
@@ -26,21 +27,24 @@ typedef struct s_token		t_token;
 typedef struct s_lexer		t_lexer;
 typedef struct s_cmd_len	t_cmd_len;
 
+typedef struct s_cmd_len
+{
+	size_t		len;
+	int			stauts;
+	t_cmd_len	*next;
+}	t_cmd_len;
+
 typedef struct s_token
 {
 	char	*data;
 	int		type;
 	t_token	*next;
-}		t_token;
+}	t_token;
 
-t_token	*token_new(void);
-int	get_char_type(char c);
-bool	compare_redirect(char *str);
-int	check_return_status(t_token **token, int status);
-int	check_status(int char_type, int status);
-int	join_return_status(t_token **token, char *str, int char_type, int status);
-int	assign_general(t_token **token, char *input, int char_type);
-int	check_token_return_status(t_token **token, char *input, int char_type, int status);
-int	lexer_build(char *input, t_token **lexerbuf);
+typedef struct s_lexer
+{
+	t_token	*list_token;
+	int		num_token;
+}			t_lexer;
 
 #endif
